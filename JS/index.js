0db8bbc7d964fc76ed8bd34b8a6d58707febb220
ivan-vz <ivanveci28@gmail.com>
2023-05-20 02:29:01 -0300

    Little upgrade of the HTML and CSS Addition of the code to paint, eraser, clean all and change the color with an input

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 let paper = document.querySelector("#paper");
-let allSquares = null;
+let paintColor = "rgb(0, 0, 0)";
+let eraserPressed = false;
 
 /* Cleaning the grid 
 const cleanGrid = () => {
@@ -26,13 +27,74 @@ const buildGrid = (side) => {
             let square = document.createElement("div");
             square.setAttribute("class", "square");
             square.style.width = `${squareSize}px`;
-            square.style.height = `${squareSize}px`
+            square.style.height = `${squareSize}px`;
+            square.addEventListener("mousedown", draw);
+            square.addEventListener("mouseover", draw);
             row.appendChild(square);
         }
         paper.appendChild(row);
     }
-
-    let squares = document.querySelectorAll(".square");
-    allSquares = squares;
 }
-buildGrid(16);
+
+let mouseClicked = false;
+document.body.onmousedown = () => (mouseClicked = true)
+document.body.onmouseup = () => (mouseClicked = false)
+
+//Function to paint the squares
+let draw = (square) => {
+    if(square.type == "mouseover" && !mouseClicked) { return }
+    square.target.style.backgroundColor = paintColor;
+}
+
+//Auxiliary function of menuEffects that unpulse all the pressed button before press the clicked one
+let changePressed = () => {
+    let alreadyPressed = document.querySelectorAll(".pressed");
+        alreadyPressed.forEach((pressed) => {
+            if(pressed.id != "grid"){
+                pressed.classList.toggle("pressed");
+            }
+        })
+}
+
+//Function to make the effect of a pressed button
+let menuEffects = document.querySelectorAll(".effect");
+menuEffects.forEach((effect) => {
+    effect.addEventListener("click", () => {
+        if(effect.id != "grid" && !effect.classList.contains("pressed")){
+            changePressed();
+        }
+        
+        effect.classList.toggle("pressed");
+    })
+})
+
+//Function to clean a square
+let eraser = document.querySelector("#eraser");
+eraser.addEventListener("click", () => {
+    if(eraserPressed) {
+        eraserPressed = false;
+        paintColor = colorPicker.value;
+    } else {
+        eraserPressed = true;
+        paintColor = "rgb(255, 255, 255)";
+    }
+});
+
+//Function to select a colour with the input color
+let colorPicker = document.querySelector("#colorPicker");
+colorPicker.addEventListener("change", () => { 
+    paintColor = colorPicker.value;
+});
+
+//Function to restart the drawing
+let clean = document.querySelector("#clean");
+clean.addEventListener("click", () => {
+    let allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square) => {
+        square.style.backgroundColor = "rgb(255, 255, 255)";
+    })
+})
+
+window.onload = () => {
+    buildGrid(16);
+}
